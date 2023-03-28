57f6bda1551b8663425f0432ef3d2c3ecaec21da
Mancoba <manq2010@gmail.com>
2023-03-28 20:56:27 +0200

    Fix linters

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,7 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
-    def random
-      @message = Message.order("RANDOM()").first
-      render json: @message 
-    end
+  def random
+    @message = Message.order('RANDOM()').first
+    render json: @message
   end
-  
+end
