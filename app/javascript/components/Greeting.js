07b3e917996e7fe1fb8cf4319224e9a5ee77b632
Mancoba <manq2010@gmail.com>
2023-03-28 20:54:31 +0200

    Add redux

--- app/javascript/components/Greeting.js
+++ app/javascript/components/Greeting.js
@@ -1,15 +1,48 @@
 import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchGreetings } from './greetingSlice';
 
-function Greeting() {
+const Greeting = () => {
 
+//  Get greetings from Redux store:
+  const greeting = useSelector((state) => state.greetingReducer);
+  const greetingsStatus = useSelector((state) => state.greetingReducer.status);
+
+  console.log('data',greeting)
+  console.log('status',greetingsStatus)
+
+
+  // Prepare Redux dispatch method:
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (greetingsStatus === 'idle') {
+      dispatch(fetchGreetings());
+    }
+  }, [greetingsStatus, dispatch]);
+
+  let content;
+
+  if (greetingsStatus === 'succeeded') {
+    content = (
+            <p>
+                { greeting.greetings.content }
+            </p>
+    )
+  } else if (greetingsStatus === 'failed') {
+    content = (
+      <>
+        <h1>message not found</h1>
+        <p>{error}</p>
+      </>
+    );
+  }
 
   return (
     <div>
-      <p>greeting</p>
+       {content}
     </div>
   );
 }
-
-console.log('Greeeting')
 
 export default Greeting;
--- app/javascript/index.js
+++ app/javascript/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { store } from './greetings';
+import { Provider } from 'react-redux';
+import store from './components/configureStore';
 import App from './App';
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <React.StrictMode>
-      {/* <Provider store={store}> */}
+      <Provider store={store}>
         <App />
-      {/* </Provider> */}
+      </Provider>
     </React.StrictMode>,
     document.getElementById('root'),
   );
